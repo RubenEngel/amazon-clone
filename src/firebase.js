99a60c7b5be6140975629ee4cd9dee9c2a367368
RubenEngel <66388962+RubenEngel@users.noreply.github.com>
2020-09-18 14:06:24 +0100

    updates

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-import firebase from "firebase"
+import firebase from 'firebase/app'
+import 'firebase/auth';
+import 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBQwA6m1jWfh3H3pF0ZOYX98lraf9coyk0",
